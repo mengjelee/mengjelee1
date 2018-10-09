103d71c4dbf4bf144f976a5c8190235a0ae0a057
mengjelee <43258760+mengjelee@users.noreply.github.com>
2018-10-09 13:26:24 +0800

    week4

--- guestbook/models.py
+++ guestbook/models.py
@@ -3,8 +3,12 @@ from django.conf import settings
 # Create your models here.
 
 class TextMessage(models.Model):
-	talker = models.CharField(max_length = 20,blank = False)
+	talker = models.CharField(max_length = 10,blank = False)
 	message = models.CharField(max_length = 10,blank = True)
 	def __str__(self):
 		return self.talker + ' ' + self.message
-
+class TextMessage1(models.Model):
+	talker1 = models.CharField(max_length = 10,blank = False)
+	message1 = models.CharField(max_length = 10,blank = True)
+	def __str__(self):
+		return self.talker1 + ' ' + self.message1
