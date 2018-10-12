f41c481998b2abae7f7cd6822dbcb74f4a3488fe
mengjelee <43258760+mengjelee@users.noreply.github.com>
2018-10-12 10:45:03 +0800

    week4_before_eexporting

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': "postgres",
+         'NAME': "testdb",
          'USER':"postgres",
          'PASSWORD':"mjbbo4i7",
          'HOST':"",
