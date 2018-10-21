5bdbd5d0047a02ce17d26d479337cef1b9763dd3
mengjelee <43258760+mengjelee@users.noreply.github.com>
2018-10-21 21:05:01 +0800

    week6 very first

--- guestbook/models.py
+++ guestbook/models.py
@@ -7,8 +7,8 @@ class TextMessage(models.Model):
 	message = models.CharField(max_length = 50,blank = True)
 	def __str__(self):
 		return self.talker + ' ' + self.message
-class TextMessage1(models.Model):
-	talker1 = models.CharField(max_length = 10,blank = False)
-	message1 = models.CharField(max_length = 10,blank = True)
-	def __str__(self):
-		return self.talker1 + ' ' + self.message1
+class UserData(models.Model):
+    username = models.CharField(max_length = 20,blank = False)
+    password = models.CharField(max_length = 20,blank = True)
+    def __str__(self):
+        return self.username + ' ' + self.password
